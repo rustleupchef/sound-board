30f3610ad607e8eef56f88b0d7472d41f3a50b3c
rustleupchef <rustle.up.chef@gmail.com>
2025-04-08 21:23:28 -0500

    added exit code and ctrl + u detection

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import os
 from playsound import playsound
 from time import sleep
+from pynput import keyboard
+import time
+
+last_played = 0.0
+chain = []
+display = False
+
+def on_press(key):
+    global last_played, display, chain
+
+    chain.append(key)
+
+    if len(chain) < 1:
+        return
+    
+    if time.time() - last_played < 0.2:
+        if keyboard.Key.ctrl in chain or keyboard.KeyCode.from_char('u') in chain:
+            display = not display
+            print(display)
+        elif keyboard.Key.ctrl in chain or keyboard.KeyCode.from_char('m') in chain:
+            return False
+    
+    chain.clear()
+    last_played = time.time()
 
 def getSoundPaths(directory = "") -> list:
     sound_files = []
@@ -15,8 +39,8 @@ def main() -> None:
         print("No files found.")
         return
     
-    index = int(input())
-    playsound(sound_files[index])
-
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+        
 if __name__ == "__main__":
     main()
